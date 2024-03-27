f48024923744d75e0cb4e7ce0943329aa5448b81
MCMANIRX <mcserv.irx@gmail.com>
2024-03-27 16:06:46 -0400

    fix

--- C/experimental/inc/adc/battery_monitor.c
+++ C/experimental/inc/adc/battery_monitor.c
@@ -21,8 +21,6 @@ void get_battery_percentage(uint8_t *percentage){
     }
 
 void get_battery_voltage(float *fvoltage){
-        const float vmin =   2.25;
-        const float vmax =   3.0;
         const float conversion_factor = 3.3f / (1 << 12);
 
         uint16_t voltage = adc_read()+ADC_OFFSET;
